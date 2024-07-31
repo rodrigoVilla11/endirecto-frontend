3db96a6d4aa098f600575e1859a47c6b697f8869
Rodrigo Villarreal <villarrealrodrigo.n@gmail.com>
2024-07-31 09:45:54 -0300

    Agregado MyProfile

--- src/app/profile/my-profile/page.tsx
+++ src/app/profile/my-profile/page.tsx
@@ -1,28 +1,32 @@
 import React from "react";
-import Input from "@/app/components/components/Input";
 import Header from "@/app/components/components/Header";
 import { FaKey } from "react-icons/fa6";
 
 const page = () => {
-
   const headerBody = {
-    buttons: [{
+    buttons: [
+      {
         logo: <FaKey />,
         title: "Change Password",
-      },],
-    filters: [
-      {
-        content: <Input placeholder={"Search..."} />,
       },
     ],
-    results: "1 Results",
+    filters: [],
+    results: "",
   };
 
   return (
     <div className="gap-4">
       <h3 className="font-bold p-4">MY PROFILE</h3>
       <Header headerBody={headerBody} />
-     
+      <div className="h-auto m-5 bg-white p-4 flex flex-col justify-between text-sm gap-8">
+        <p>Id: <span className="font-bold">5ea961d5-756d-4c51-bebb-2f9204b01846</span></p>
+        <p>Name: <span className="font-bold">Rodrigo Villarreal</span></p>
+        <p>Role: <span className="font-bold">ADMINISTRADOR</span></p>
+        <p>Email: <span className="font-bold">rodrigo@gmail.com</span></p>
+        <p>Branch: <span className="font-bold">001 - DEP YRIGOYEN</span></p>
+        {//AGREGAR MAS INFO PARA CUANDO SE SELECCIONA EL CLIENTE
+        }
+      </div>
     </div>
   );
 };
