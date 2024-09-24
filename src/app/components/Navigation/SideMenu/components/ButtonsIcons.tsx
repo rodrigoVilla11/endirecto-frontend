fea135398c167c7ee9dfacd6a632271666ceb3ae
Rodrigo Villarreal <villarrealrodrigo.n@gmail.com>
2024-09-24 09:30:52 -0300

    hot-fixes

--- src/app/components/Navigation/SideMenu/components/ButtonsIcons.tsx
+++ src/app/components/Navigation/SideMenu/components/ButtonsIcons.tsx
@@ -5,16 +5,28 @@ import { IoIosArrowDown } from "react-icons/io";
 import { useSideMenu } from "@/app/context/SideMenuContext";
 import { GoDotFill } from "react-icons/go";
 
-const ButtonsIcons = ({ icon, isOpen }: any) => {
+interface Icon {
+  icon: React.ReactNode;
+  name: string;
+  path?: string;
+  subCategories?: { name: string; path: string }[];
+}
+
+interface ButtonsIconsProps {
+  icon: Icon;
+  isOpen: boolean;
+}
+
+const ButtonsIcons: React.FC<ButtonsIconsProps> = ({ icon, isOpen }) => {
   const { setIsOpen } = useSideMenu();
   const router = useRouter();
   const [showSubCategories, setShowSubCategories] = useState(false);
 
   const toggleSubCategories = () => {
-    setShowSubCategories(!showSubCategories);
+    setShowSubCategories((prev) => !prev);
   };
 
-  const handleRedirect = (path: string, event: React.MouseEvent<Element, MouseEvent>) => {
+  const handleRedirect = (path: string, event: React.MouseEvent) => {
     event.stopPropagation();
     if (path) {
       router.push(path);
@@ -24,7 +36,7 @@ const ButtonsIcons = ({ icon, isOpen }: any) => {
   return (
     <div
       className="relative flex items-center text-2xl text-white group"
-      onClick={icon.subCategories ? toggleSubCategories : () => handleRedirect(icon.path, new MouseEvent('click'))}
+      onClick={icon.subCategories ? toggleSubCategories : (event) => handleRedirect(icon.path || "", event)}
     >
       {!isOpen ? (
         <div className="flex items-center text-center flex-col cursor-pointer">
@@ -39,21 +51,19 @@ const ButtonsIcons = ({ icon, isOpen }: any) => {
           <div className="flex items-center gap-4">
             <div className="text-left hover:cursor-pointer">{icon.icon}</div>
             <div className="text-sm hover:cursor-pointer">{icon.name}</div>
-            {icon.subCategories && (
-              <IoIosArrowDown className="text-sm cursor-pointer" />
-            )}
+            {icon.subCategories && <IoIosArrowDown className="text-sm cursor-pointer" />}
           </div>
           {isOpen && showSubCategories && (
             <div className="bg-header-color rounded-md px-2 w-48 text-start">
               <ul>
-                {icon.subCategories.map((subcategory: any, index: number) => (
+                {icon.subCategories && icon.subCategories.map((subcategory, index) => (
                   <li
                     key={index}
                     className="text-sm p-1 hover:cursor-pointer"
-                    onClick={(event: React.MouseEvent<Element, MouseEvent>) => handleRedirect(subcategory.path, event)}
+                    onClick={(event) => handleRedirect(subcategory.path, event)}
                   >
                     <div className="flex gap-1 text-center text-xs">
-                      <GoDotFill />{subcategory.name}
+                      <GoDotFill /> {subcategory.name}
                     </div>
                     <hr />
                   </li>
