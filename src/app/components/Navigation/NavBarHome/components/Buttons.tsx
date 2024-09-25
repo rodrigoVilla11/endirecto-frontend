79d0e34c04f5b6d1a8026a516457f62a65567047
Rodrigo Villarreal <villarrealrodrigo.n@gmail.com>
2024-09-25 13:07:49 -0300

    Agregado routeo

--- src/app/components/Navigation/NavBarHome/components/Buttons.tsx
+++ src/app/components/Navigation/NavBarHome/components/Buttons.tsx
@@ -1,8 +1,15 @@
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import { BiSearchAlt } from "react-icons/bi";
 import { GiUsaFlag } from "react-icons/gi";
 
 const Buttons = () => {
+  const router = useRouter();
+  const handleRedirect = (path: string) => {
+    if (path) {
+      router.push(path);
+    }
+  };
   return (
     <div className='flex items-center justify-between text-sm text-white gap-6'>
         <button>Home</button>
@@ -11,7 +18,7 @@ const Buttons = () => {
         <button>Articles</button>
         <button>Contact</button>
         <button className='text-xl'><GiUsaFlag/></button>
-        <button>Sign In</button>
+        <button onClick={()=>handleRedirect("/login")}>Sign In</button>
         <button className='text-xl'><BiSearchAlt/></button>
 
     </div>
