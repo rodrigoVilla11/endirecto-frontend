098aff10f34c5fc2f019d4b3691bf15a4acf8968
rodrigovilla11 <villarrealrodrigo.n@gmail.com>
2024-12-26 21:54:25 -0300

    ready

--- src/app/components/Catalogue/components/Articles/components/ArticleName.tsx
+++ src/app/components/Catalogue/components/Articles/components/ArticleName.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ArticleName = ({name, id, noName} : any) => {
   return (
-    <div className={`p-4 ${noName ? "h-12" : "h-24"}`}>
+    <div className={`p-4 ${noName ? "h-12  rounded-lg" : "h-24"} `}>
       <p className="text-xs font-semibold mb-2">{id}</p>
       {!noName && <p className="text-xs text-gray-500 mb-2">{name}</p>}
     </div>
--- src/app/components/Catalogue/components/Search.tsx
+++ src/app/components/Catalogue/components/Search.tsx
@@ -6,7 +6,7 @@ import { useGetArticlesQuery } from "@/redux/services/articlesApi";
 import ArticleName from "./Articles/components/ArticleName";
 
 const ArticleSearchResults = ({ query }: { query: string }) => {
-  const limit = 4;
+  const limit = 6;
   const {
     data: articles,
     error,
@@ -22,10 +22,10 @@ const ArticleSearchResults = ({ query }: { query: string }) => {
   }
 
   return (
-    <div className="fixed top-16 left-20 right-0 bg-white shadow-lg p-6 z-50 h-auto max-h-[80vh] flex flex-col justify-between items-center rounded-lg border-2 border-gray-200">
+    <div className="fixed top-16 left-20 right-0 bg-[rgba(0,0,0,0.8)] shadow-lg p-6 z-50 h-auto max-h-[80vh] flex flex-col justify-between items-center rounded-lg border-2 border-black">
       {/* Título y botón de comparar */}
       <div className="flex justify-between items-center w-full mb-4">
-        <h3 className="text-lg font-bold text-gray-800">
+        <h3 className="text-lg font-bold text-white">
           Resultados para "{query}":
         </h3>
       </div>
@@ -42,7 +42,7 @@ const ArticleSearchResults = ({ query }: { query: string }) => {
       <div className="flex flex-wrap justify-center gap-6 overflow-auto">
         {articles &&
           articles.map((article: any) => (
-            <div key={article.id} className="relative w-72 max-w-xs">
+            <div key={article.id} className="relative w-44 max-w-xs">
               {/* Contenido del artículo */}
               <div className="relative flex flex-col justify-center items-center shadow-lg bg-white cursor-pointer rounded-lg hover:shadow-xl transition-all duration-300">
                 <img
@@ -50,7 +50,7 @@ const ArticleSearchResults = ({ query }: { query: string }) => {
                   alt={article.name}
                   className="w-32 h-40 object-contain rounded-lg"
                 />
-                <div className="bg-gray-100 px-4 py-2 w-full text-center">
+                <div className="bg-gray-100 px-4 py-2 w-full text-center rounded-lg">
                   <ArticleName
                     name={article.name}
                     id={article.id}
@@ -62,7 +62,7 @@ const ArticleSearchResults = ({ query }: { query: string }) => {
           ))}
       </div>
 
-      <button className="mt-6 bg-blue-600 text-white px-8 py-3 rounded-lg hover:bg-blue-700 transition duration-300 transform hover:scale-105 focus:outline-none shadow-lg">
+      <button className="mt-6 bg-white text-black px-8 py-3 rounded-lg hover:bg-blue-700 transition duration-300 transform hover:scale-105 focus:outline-none shadow-lg">
         Ver más...
       </button>
     </div>
