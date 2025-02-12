40eb38deb667ce4c184888dfe6dc6ffe8a0e3dce
Rodrigo Villarreal <villarrealrodrigo.n@gmail.com>
2025-02-12 19:24:15 -0300

    ready

--- src/app/components/Catalogue/components/Articles/components/Description/components/TableAppOfArticles.tsx
+++ src/app/components/Catalogue/components/Articles/components/Description/components/TableAppOfArticles.tsx
@@ -3,14 +3,13 @@ export default function VehicleTable({ vehicles }: any) {
     <table className="min-w-full bg-white">
       <thead>
         <tr className="text-xs">
-          <th className="py-2 px-2 bg-gray-200 text-left">Marca</th>
-          <th className="py-2 px-2 bg-gray-200 text-left">Modelo</th>
-          <th className="py-2 px-2 bg-gray-200 text-left">Motor</th>
-          <th className="py-2 px-2 bg-gray-200 text-left">Año</th>
+          <th className="py-2  bg-gray-200 text-left">Marca</th>
+          <th className="py-2  bg-gray-200 text-left">Modelo</th>
+          <th className="py-2  bg-gray-200 text-left">Motor</th>
+          <th className="py-2  bg-gray-200 text-left">Año</th>
         </tr>
       </thead>
       <tbody className="text-xs">
-
         {vehicles.map((vehicle: any, index: any) => (
           <tr
             key={index}
@@ -18,10 +17,10 @@ export default function VehicleTable({ vehicles }: any) {
               index % 2 === 0 ? "bg-white" : "bg-gray-50"
             }`}
           >
-            <td className="border-t py-2 px-2">{vehicle.brand}</td>
-            <td className="border-t py-2 px-2">{vehicle.model}</td>
-            <td className="border-t py-2 px-2">{vehicle.engine}</td>
-            <td className="border-t py-2 px-2">{vehicle.year}</td>
+            <td className="border-t py-2 ">{vehicle.brand}</td>
+            <td className="border-t py-2 ">{vehicle.model}</td>
+            <td className="border-t py-2 ">{vehicle.engine}</td>
+            <td className="border-t py-2 ">{vehicle.year}</td>
           </tr>
         ))}
       </tbody>
