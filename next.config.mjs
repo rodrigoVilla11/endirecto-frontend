09b5c8b99a507efa10da243124b33aa96cb91d98
rodrigovilla11 <villarrealrodrigo.n@gmail.com>
2025-02-17 13:03:31 -0300

    some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    },
+  };
+  
+  export default nextConfig;
+  
